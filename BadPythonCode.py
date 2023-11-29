6ae20aac65d55939fe1edb25ea24520384432078
Ella Mäkelä <ella@makela.co>
2023-11-29 10:42:45 +0200

    exercise done :)

---
+++ BadPythonCode.py
@@ -0,0 +1,68 @@
+import json
+import random
+import os
+import sys
+
+
+class exampleClass:
+def **init**(self, val1, val2):
+
+
+self.val1 = val1
+self.val2 = val2
+
+
+def add(self):
+    sum = self.val1 + self.val2
+    print("Sum is:", sum)
+
+
+def another_function():
+
+
+print("This function does nothing useful")
+
+example = exampleClass(5, 10)
+example.add()
+
+for i in range(0, 10):
+print(i)
+
+
+def unused_function():
+
+
+pass
+
+# Badly formatted dictionary
+
+example_dict = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
+
+# Unused imports
+
+
+# Too many blank lines
+
+# Unused variables
+
+unused_var = 123
+
+
+def yet_another_function():
+
+
+    # Inconsistent indentation
+if True:
+print("This block has inconsistent indentation")
+
+
+def function_with_too_many_arguments(a, b, c, d, e, f, g, h, i, j, k):
+
+
+pass
+
+if **name **= ="**main**":
+another_function()
+unused_function()
+yet_another_function()
+function_with_too_many_arguments(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
